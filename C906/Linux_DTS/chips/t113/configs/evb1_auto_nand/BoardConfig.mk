fc0154d66cbf3318b04d4218af4a7abe7b2abe5d
GammaKinematics <gamma.kinematics@gmail.com>
2025-11-17 16:08:11 +0700

    Add Linux DTS

---
+++ C906/Linux_DTS/chips/t113/configs/evb1_auto_nand/BoardConfig.mk
@@ -0,0 +1,5 @@
+LICHEE_KERN_DEFCONF:=sun8iw20p1smp_auto_nand_defconfig
+LICHEE_BRANDY_DEFCONF:=sun8iw20p1_auto_nand_defconfig
+LICHEE_NO_RAMDISK_NEEDED:=y
+LICHEE_BUILDING_SYSTEM:=buildroot
+LICHEE_BR_VER:=201902
